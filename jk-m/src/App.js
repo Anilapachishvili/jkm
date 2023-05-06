24dc657f680500ebf433f2a44657253b4db4c695
Ani <anilafachishvili2020@gmail.com>
2023-05-06 10:24:45 -0700

    cr

---
+++ jk-m/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h>aaamsdofkdsllllfd</h>
+    </div>
+  );
+}
+
+export default App;
